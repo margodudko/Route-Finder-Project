a9e6b66b2ff9d87bf516c23056289bdf0dd10366
margodudko <chisa11@yandex.ru>
2022-10-14 11:24:50 +0300

    добавлены фикстуры

--- travel/settings/__init__.py
+++ travel/settings/__init__.py
@@ -4,29 +4,5 @@ try:
 except ImportError:
     pass
 
-from django.conf.urls import url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
-urlpatterns = [
-    url(
-        'accounts/password_reset/',
-        PasswordResetView.as_view(),
-        name='password_reset'
-    ),
-    url(
-        'accounts/password_reset_done/',
-        PasswordResetDoneView.as_view(),
-        name='password_reset_done'
-    ),
-    url(
-        'accounts/password_reset_confirm/',
-        PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'
-    ),
-    url(
-        'accounts/password_reset_complete/',
-        PasswordResetCompleteView.as_view(),
-        name='password_reset_complete'
-    ),
-]
